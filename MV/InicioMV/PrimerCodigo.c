10c73490fe1735f6df1c5d2dd929e24692389d7f
Octavio44635 <81695953+Octavio44635@users.noreply.github.com>
2025-04-04 16:32:32 -0300

    Co-authored-by: LuchoJula12 <LuchoJula12@users.noreply.github.com>

--- MV/InicioMV/PrimerCodigo.c
+++ MV/InicioMV/PrimerCodigo.c
@@ -10,53 +10,52 @@ typedef struct{
 
 typedef struct{
     char memoria[16384];
-    int registros[16];
+    int registros[16];  //Del elemento 8 al 15 son valores enteros, los demas son referencias a memoria
     TablaS TSeg[2];} mv;
 
 void lectura(mv*);
 void CargaRegistros(mv*, int);
 
-void SYS(mv*);
-void JMP(mv*);
-void JZ(mv* );
-void JP(mv* );
-void JZ(mv* );
-void JNZ(mv*);
-void JNP(mv* );
-void JNN(mv* );
-void NOT(mv* );
-void EMPTY(mv*);
-void STOP(mv* );
-void MOV(mv* );
-void ADD(mv* );
-void SUB(mv* );
-void SWAP(mv*);
-void MUL(mv* );
-void DIV(mv* );
-void CMP(mv* );
-void SHL(mv* );
-void SHR(mv* );
-void AND(mv* );
-void OR(mv* );
-void XOR(mv* );
-void LDL(mv* );
+void SYS(mv*,int, int );
+void JMP(mv*,int, int );
+void JZ(mv*,int, int );
+void JP(mv*,int, int );
+void JZ(mv*,int, int );
+void JNZ(mv*,int, int );
+void JNP(mv*,int, int );
+void JNN(mv*,int, int );
+void NOT(mv*,int, int );
+void EMPTY(mv*,int, int );
+void STOP(mv*,int, int );
+void MOV(mv*,int, int );
+void ADD(mv*,int, int );
+void SUB(mv*,int, int );
+void SWAP(mv*,int, int );
+void MUL(mv*,int, int );
+void DIV(mv*,int, int );
+void CMP(mv*,int, int );
+void SHL(mv*,int, int  );
+void SHR(mv*,int, int  );
+void AND(mv*,int, int );
+void OR(mv*,int, int );
+void XOR(mv*,int, int  );
+void LDL(mv*,int, int  );
 void LDH(mv*,int, int );
 void RND(mv*,int, int );
 
-void LeeCS(mv*, void**); //Chequear esto del doble puntero //////////////////////////////////
+void LeeCS(mv*);
 
-//void REGISTRO(int * );
-void INMEDIATO(int * );
-void MEMORIA(int * );
-void NULO(int * );
+void SACAREGISTRO(mv, int *, int *);
+void INMEDIATO(mv, int *, int *);
+void MEMORIA(mv, int *, int * );
+void NULO(mv, int *, int *);
 
 int main(){
-    void (*Funciones[32])(mv*, int, int) = {SYS,JMP,JZ,JP,JZ,JNZ,JNP,JNN,NOT,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,STOP,MOV,ADD,SUB,SWAP,MUL,DIV,CMP,SHL,SHR,AND,OR,XOR,LDL,LDH,RND,EMPTY}; //Las funciones del vector deben recibir los mismos parametros
+     //Las funciones del vector deben recibir los mismos parametros
 
     mv MV; //Variable maquina virtual
-    int tamCS;
     lectura (&MV); //Llamada a la funcion lectura
-    LeeCS(&MV, Funciones);
+    LeeCS(&MV);
 
 
     return 0;
@@ -73,10 +72,12 @@ void CargaRegistros(mv* MV, int tamCS){
     (*MV).registros[1] = 0x10000; //DS
     (*MV).registros[5] = ((*MV).TSeg[0].Base << 16) & 0x0; //IP
 
+
     
     //Los demas registros se inicializan al usarlos
 }
 void lectura(mv* MV){
+
     FILE *arch = fopen("sample.vmx", "rb");
     if( arch != NULL){
         fread((*MV).memoria, sizeof(char), 8, arch); //Se leen bytes de forma arbitraria, los primeros 7 son el header, siendo los ultimos 2 el tamaño 
@@ -99,47 +100,155 @@ void lectura(mv* MV){
 }
 
 
-void LeeCS (mv *MV, void (Funciones[])(mv*, int, int)){
-    void (*FuncOperandos[4])(mv, int *, int *) = {NULO,REGISTRO,INMEDIATO,MEMORIA};
+void LeeCS (mv *MV){
+    void (*Funciones[32])(mv*, int, int, char ) = {EMPTY,JMP,JZ,JP,JZ,JNZ,JNP,JNN,NOT,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,STOP,MOV,ADD,SUB,SWAP,MUL,DIV,CMP,SHL,SHR,AND,OR,XOR,LDL,LDH,RND,EMPTY};
+    void (*FuncOperandos[4])(mv, int *, int *) = {NULO,SACAREGISTRO,INMEDIATO,MEMORIA};
+
+
     char instruccion;
     int opA,opB,operacion,IPaux, cont=0;
+
+
     IPaux=(*MV).registros[5];
-    while ((*MV).registros[5]<=(*MV).TSeg[0].Tamanio){  //MV.REGISTROS 5 = IP
+    while ((*MV).registros[5]<=4){  //MV.REGISTROS 5 = IP (*MV).TSeg[0].Tamanio
       instruccion = (*MV).memoria[IPaux]; //levanto el dato del CS apuntado por IP
       opB=(instruccion&0xC0)>>6;    //4 valores, 2 bits
       opA=(instruccion&0x30)>>4;
       operacion=(instruccion &0x1F);
-
-      FuncOperandos[opB](&cont);
-      //Suma al contador 1
-      //Saca los bytes de la memoria de la posicion [Mv.registro[5] + cont]
+      //En instruccion la receta de la orden
+      //xx x_x_xxxx
+      FuncOperandos[opB](*MV, &cont, &opB);
+      
+      FuncOperandos[opA](*MV, &cont, &opA);
+      printf("opA: %d, opB: %d\n", opA, opB);
+      //Funciones[operacion](MV, opA, opB); //Llama a la funcion correspondiente
       
       IPaux= IPaux + cont;
-      FuncOperandos[opA](&cont);
-      IPaux= IPaux + cont;
-      Funciones[operacion](MV, opA, opB); //Llama a la funcion correspondiente
-      
       (*MV).registros[5]=IPaux; //Graba en el IP, la ultima instruccion leida
     
     }
 
 }
 
-void REGISTRO (mv MV,int *cont, int *op){
-    char Sacamemoria;
-    *cont+=1;
-    Sacamemoria= MV.registros[5] + cont;
-       
-    
+void SACAREGISTRO (mv MV,int *cont, int *op){
+    char PosRegistro, Sector;
+    *op = MV.memoria[MV.registros[5] + *cont + 1]; //Posicion del CS ####################
+    //Sacamemoria = byte a leer
+    *cont += 1;
+} // opA = 1100 01 00
+
+void INMEDIATO (mv MV,int *cont, int *op){
+  short int inmediato;
+  inmediato = MV.memoria[MV.registros[5]+*cont + 2];
+  inmediato = (MV.memoria[MV.registros[5]+*cont + 1] << 8) | inmediato;
+  *op = inmediato;
+  *cont += 2; //Aumento el contador
 }
 
-void NULO (int *op){
+void MEMORIA (mv MV, int *cont, int *op){
+    short int offset, basereg, inmediato;
+    char codreg;
+    int PosMem;
+
+    //primero se lee un inmediato, dos bytes
+
+    //Funcion INMEDIATO
+    inmediato = MV.memoria[MV.registros[5]+*cont + 2];
+    inmediato = (MV.memoria[MV.registros[5]+*cont + 1] << 8) | inmediato;
+    *cont += 2; 
+    //
+    
+    codreg= (MV.memoria[MV.registros[5]+ *cont + 1]>>4); //Codigo del registro en el vector
+    
+    basereg = MV.registros[codreg] >> 16;
+    offset= MV.registros[codreg] & 0x00FF;
+
+    //PosMem = MV.TSeg[basereg].Base + offset + inmediato;
+
+    //*op = MV.memoria[PosMem];
+    *op = MV.TSeg[basereg].Base + offset + inmediato;
+}
+
+void NULO (mv MV, int *cont, int *op){
     *op = -1;
 }
 
-void EMPTY(mv* MV){
+void EMPTY(mv* MV, int opA, int opB, char operacion){
     //No hace nada
 }
 
+//SYS,JMP,JZ,JP,JZ,JNZ,JNP,JNN,NOT,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,STOP,MOV,ADD,SUB,SWAP,MUL,DIV,CMP,SHL,SHR,AND,OR,XOR,LDL,LDH,RND,EMPTY
 
 
+void MOV (mv* MV, int opA, int opB, char operacion){
+    char tipoA=(operacion >> 4)
+}
+
+void ADD (mv* MV, int opA, int opB, char operacion){
+    (*MV).memoria[opA]+=opB;
+}
+
+void JMP (mv* MV, int opA, int opB, char operacion){
+    //JMP
+    (*MV).registros[5] = opA;
+}
+void JZ (mv* MV, int opA, int opB, char operacion){
+    //JZ
+    
+    if (((*MV).registros[8]) & 0xD000 == 0){ //CC
+        (*MV).registros[5] = opA; //IP
+    }
+}
+
+void JN (mv* MV, int opA, int opB, char operacion){
+    //JN
+    
+    if (((*MV).registros[8]) & 0xD000 < 0){ //CC
+        (*MV).registros[5] = opA; //IP
+    }
+}
+
+void JNZ (mv* MV, int opA, int opB, char operacion){
+    //JN
+    
+    if (((*MV).registros[8]) & 0xD000 != 0){ //CC
+        (*MV).registros[5] = opA; //IP
+    }
+}
+
+
+void JNP (mv* MV, int opA, int opB, char operacion){
+    //JN
+    
+    if (((*MV).registros[8]) & 0xD000 <= 0){ //CC
+        (*MV).registros[5] = opA; //IP
+    }
+}
+
+
+void JNN (mv* MV, int opA, int opB, char operacion){
+    //JN
+    
+    if (((*MV).registros[8]) & 0xD000 >=  0){ //CC
+        (*MV).registros[5] = opA; //IP
+    }
+}
+
+
+int LDH (mv* MV, int opA, int opB){
+    int aux=0x00000000;
+    opA=OpA & 0xFF00 //quedaria 11111111 11111111 000000....
+    aux=aux | (opB & 0x00FF);     
+    return aux;
+}
+
+int LDL (mv* MV, int opA, int opB){
+    int aux=0x00000000;
+    aux=opA<<16;
+    aux=aux | (opB & 0x00FF);    
+    return aux;
+}
+
+int RND (mv* MV, int opA, int opB){
+    return= rand() % (opB + 1);
+}
